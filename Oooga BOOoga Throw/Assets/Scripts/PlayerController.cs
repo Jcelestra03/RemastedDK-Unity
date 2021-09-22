3a75ec0df1371f446bf184c5ddc9b6dfd491d4ef
Jcelestra03 <82464830+Jcelestra03@users.noreply.github.com>
2021-09-22 12:06:44 -0700

    Player, and PlayerController + Movement
    moving left and right and jumping.

---
+++ Oooga BOOoga Throw/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class PlayerController : MonoBehaviour
+{
+
+    private Rigidbody2D myRB;
+    private Vector2 velocity;
+    public int movementspeed = 5;
+    public float jumpheight = 5f;
+    private Vector2 groundDetection;
+    public float groundDetectDistance = .15f;
+    private Quaternion zero;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myRB = GetComponent<Rigidbody2D>();
+        zero = new Quaternion();
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        velocity = myRB.velocity;
+        velocity.x = Input.GetAxisRaw("Horizontal") * movementspeed;
+
+        groundDetection = new Vector2(transform.position.x, transform.position.y - 1.1f);
+
+        if (Input.GetKeyDown(KeyCode.Space) && Physics2D.Raycast(groundDetection, Vector2.down, groundDetectDistance))
+        {
+            velocity.y = jumpheight;
+        }
+
+        myRB.velocity = velocity;
+    }
+}
